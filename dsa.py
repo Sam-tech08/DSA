f1b68c10fe3d53d161f3d31f90d812929d653374
Samyak.D.Karekar <iamsam08122005@gmail.com>
2025-09-17 20:43:32 +0530

    first leet code problem

--- dsa.py
+++ dsa.py
@@ -1,1 +1,15 @@
-print("hello github")
+
+'''this is to check for tow numbers are ame or not'''
+class Solution(object):
+    def twoSum(self, nums, target):
+        sum =0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                sum = nums[i]+nums[j]
+                if sum == target:
+                    return[i,j]
+                
+
+S =Solution()
+t= S.twoSum([3,2,4],6)
+print(t)
