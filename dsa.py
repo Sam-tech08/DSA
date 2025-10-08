f27294b158c53ddd00b6780342f591c61732777f
Samyak.D.Karekar <iamsam08122005@gmail.com>
2025-10-08 19:27:35 +0530

    selection sort

--- dsa.py
+++ dsa.py
@@ -9,7 +9,48 @@ class Solution(object):
                 if sum == target:
                     return[i,j]
                 
+'''Given the integer day denoting the day number, print on the screen which day of the week it is.
+ Week starts from Monday and for values greater than 7 or less than 1, print Invalid.
+Ensure only the 1st letter of the answer is capitalised. '''
 
-S =Solution()
-t= S.twoSum([3,2,4],6)
-print(t)
+
+class Find_day:
+    def Whichweek(self,day):
+        if day == 1:
+            return "Monday"
+        elif day == 2:
+            return "Tuesday"
+        elif day == 3:
+            return "Wednesday"
+        elif day == 4:
+            return "Thursday"
+        elif day == 5:
+            return "Friday"
+        elif day == 6:
+            return "Saturday"
+        elif day == 7:
+            return "Sunday"
+        else:
+            return "Invalid"
+d = Find_day()
+day = d.Whichweek(5)
+print(day)
+
+
+class Sort_Solution:
+    def selectionsort(self,nums):
+        for i in range(len(nums)-1):
+            if nums[i] <= nums[i+1]:
+                continue
+            else:
+                for j in range(i+1,len(nums)):
+                    if nums[i] > nums[j]:
+                        nums[i],nums[j] = nums[j],nums[i]
+                        
+        return nums
+    
+        
+    
+s = Sort_Solution()
+nums = [7,4,1,5,3]
+print(s.selectionsort(nums))
